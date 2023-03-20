6b54719d9ff56392e3c9f960f7b2216412425ce5
Jhony Freitas <jhony00._@hotmail.com>
2023-03-20 23:03:49 +0000

    Criando pastas do projeto

---
+++ src/components/cards/index.js
@@ -0,0 +1,10 @@
+import { Card } from "../card";
+import { Container } from "./styles";
+
+export const Cards = () => {
+    return(
+        <Container>
+            <Card />
+        </Container>
+    )
+}
---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import { Cards } from "../components/cards";
+import { Container } from "./styles";
+ 
+export const App = () => {
+	return (
+		<Container>
+			<Cards />
+		</Container>
+	);
+}
